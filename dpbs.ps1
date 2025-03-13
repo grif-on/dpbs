394c3fc894f0370629cebc8252bda54ffccd55d1
Grif_on <91139654+grif-on@users.noreply.github.com>
2025-03-13 12:59:53 +0300

    Fixed path to licence file

--- dpbs.ps1
+++ dpbs.ps1
@@ -3,8 +3,8 @@ Remove-Item -Path ".\Delirium.zip" -Force
 New-Item -Path "temp" -ItemType Directory
 
 Set-Location "./temp"
-& "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf=".\licence.plist" Windows PackageZip
-# & "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf=".\licence.plist" --runtime=YYC Windows PackageZip
+& "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" Windows PackageZip
+# & "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" --runtime=YYC Windows PackageZip
 Set-Location ".."
 
 Copy-Item -Path "V:\git\Mapping" -Destination ".\temp\output\Delirium\Mapping" -Recurse -Force
