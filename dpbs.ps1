eca96e34a73fc1417a78026007878534684bd31c
Grif_on <91139654+grif-on@users.noreply.github.com>
2025-03-24 22:25:08 +0300

    Added note about empty file name workaround

--- dpbs.ps1
+++ dpbs.ps1
@@ -5,6 +5,8 @@ New-Item -Path "temp" -ItemType Directory
 Set-Location "./temp"
 & "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" Windows PackageZip
 # & "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" --runtime=YYC Windows PackageZip
+Write-Host "`nPlease , don't mind the `"Empty file name is not legal`" error ."
+Write-Host "As far as i can tell , this is the only way to make gamemaker skip zipping of files :)`n"
 Set-Location ".."
 
 Copy-Item -Path "V:\git\Mapping" -Destination ".\temp\output\Delirium\Mapping" -Recurse -Force
