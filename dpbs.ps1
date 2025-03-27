dc551ab200035feac6f666d11d4e330a0dbefc11
Grif_on <91139654+grif-on@users.noreply.github.com>
2025-03-27 16:36:49 +0300

    Ensure that current location resetted even if compilation is aborted

--- dpbs.ps1
+++ dpbs.ps1
@@ -4,11 +4,14 @@ Remove-Item -Path ".\Delirium.zip" -Force -ErrorAction Ignore
 Remove-Item -Path ".\temp\output" -Recurse -Force -ErrorAction Ignore
 
 Set-Location "./temp"
-& "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" Windows PackageZip
-# & "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" --runtime=YYC Windows PackageZip
-Write-Host "`nPlease , don't mind the `"Empty file name is not legal`" error ."
-Write-Host "As far as i can tell , this is the only way to make gamemaker skip zipping of files :)`n"
-Set-Location ".."
+try {
+	& "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" Windows PackageZip
+	# & "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" --runtime=YYC Windows PackageZip
+	Write-Host "`nPlease , don't mind the `"Empty file name is not legal`" error ."
+	Write-Host "As far as i can tell , this is the only way to make gamemaker skip zipping of files :)`n"
+} finally {
+	Set-Location ".."
+}
 
 Copy-Item -Path "V:\git\Mapping" -Destination ".\temp\output\Delirium\Mapping" -Recurse -Force
 Copy-Item -Path "D:\Dropbox\Moosor" -Destination ".\temp\output\Delirium\Moosor" -Recurse -Force
