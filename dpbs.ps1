05afba9d7c5eaf9f7940691a38623c1919dbd3f7
Grif_on <91139654+grif-on@users.noreply.github.com>
2025-03-28 13:16:13 +0300

    Automated VsDevCmd.bat path supplying for Igor.exe

--- dpbs.ps1
+++ dpbs.ps1
@@ -3,10 +3,13 @@ New-Item -Path "temp" -ItemType Directory -ErrorAction Ignore
 Remove-Item -Path ".\Delirium.zip" -Force -ErrorAction Ignore
 Remove-Item -Path ".\temp\output" -Recurse -Force -ErrorAction Ignore
 
+# Igor.exe is hardcoded to read location of VsDevCmd.bat from this file
+Set-Content -NoNewline -Path "./temp/local_settings.json" -Value ("{ `"machine.Platform Settings.Windows.visual_studio_path`": `"" + "V:/Programs/VS/2022/Community/Common7/Tools/VsDevCmd.bat" + "`" }")
+
 Set-Location "./temp"
 try {
-	& "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" Windows PackageZip
-	# & "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" --runtime=YYC Windows PackageZip
+	# & "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" Windows PackageZip
+	& "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="V:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" --runtime=YYC Windows PackageZip
 	Write-Host "`nPlease , don't mind the `"Empty file name is not legal`" error ."
 	Write-Host "As far as i can tell , this is the only way to make gamemaker skip zipping of files :)`n"
 } finally {
@@ -20,3 +23,4 @@ Copy-Item -Path "D:\Dropbox\Characters" -Destination ".\temp\output\Delirium\Cha
 & "D:\Programs\7-Zip\7z.exe" a ".\Delirium.zip" ".\temp\output\Delirium"
 
 Remove-Item -Path ".\temp" -Recurse -Force
+Remove-Item -Path "./temp/local_settings.json" -Force
