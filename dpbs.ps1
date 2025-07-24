a0b308bf59654c5fb2e7adffe3da80adb8bf1358
Grif_on <91139654+grif-on@users.noreply.github.com>
2025-07-24 14:55:04 +0300

    Formatted try catch

--- dpbs.ps1
+++ dpbs.ps1
@@ -21,10 +21,13 @@ Remove-Item -Path ".\output" -Recurse -Force -ErrorAction Ignore
 # Igor.exe is hardcoded to read location of VsDevCmd.bat from this file
 Set-Content -NoNewline -Path "./temp/local_settings.json" -Value ("{ `"machine.Platform Settings.Windows.visual_studio_path`": `"" + "C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/Tools/VsDevCmd.bat" + "`" }")
 
-Set-Location "./temp"
 try {
+	Set-Location "./temp"
+	
 	# "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="D:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" --runtime=YYC Windows PackageZip
+	
 	& "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="D:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" Windows PackageZip
+	
 	Write-Host "`nPlease , don't mind the `"Empty file name is not legal`" error ."
 	Write-Host "As far as i can tell , this is the only way to make gamemaker skip zipping of files :)`n"
 } finally {
