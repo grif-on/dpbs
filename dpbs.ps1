fd44d1a9b4a89eb564604081b8956556080655c9
Grif_on <91139654+grif-on@users.noreply.github.com>
2025-07-25 13:22:28 +0300

    Redirected logging of standard gamemaker IDE output in to files

--- dpbs.ps1
+++ dpbs.ps1
@@ -24,9 +24,9 @@ Set-Content -NoNewline -Path "./temp/local_settings.json" -Value ("{ `"machine.P
 try {
 	Set-Location "./temp"
 	
-	# "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="D:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" --runtime=YYC Windows PackageZip
+	# "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="D:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" --runtime=YYC Windows PackageZip > compile_log_yyc.txt
 	
-	& "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="D:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" Windows PackageZip
+	& "C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160\bin\igor\windows\x64\Igor.exe" --project="D:\git\Delirium\Delirium.yyp" --rp="C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160" --lf="..\licence.plist" Windows PackageZip > compile_log_vm.txt
 	
 	Write-Host "`nPlease , don't mind the `"Empty file name is not legal`" error ."
 	Write-Host "As far as i can tell , this is the only way to make gamemaker skip zipping of files :)`n"
