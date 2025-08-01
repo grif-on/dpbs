157d9d3b37d4d405c25d4b5f53b2ed46d4857e68
Grif_on <91139654+grif-on@users.noreply.github.com>
2025-08-01 12:01:18 +0300

    Refactored -CleanUp script argument and fixed that "temp" folder didn't cleaned up when this script argument are used

--- dpbs.ps1
+++ dpbs.ps1
@@ -22,7 +22,8 @@ if (!($CompileVM -or $CompileYYC -or $CleanUp)) {
 # Ignore clean up argument if already got compilation argument(s)
 if ($CleanUp -and ($CompileVM -or $CompileYYC)) {
 	Write-Host "Note - you can't use -CleanUp and compile project at the same time !`n"
-	$CleanUp = $false
+	$CompileVM = $false
+	$CompileYYC = $false
 }
 
 if ($ConfigFilePath -eq "") {
@@ -65,7 +66,7 @@ if (!$CleanUp) {
 	Add-Member -InputObject $config -MemberType NoteProperty -Name gamemaker_runtime -Value $($runtime_path_parts -join "/")
 }
 
-if (!$CleanUp -and $config.use_assets_cache) {
+if ($config.use_assets_cache -and !$CleanUp) {
 	if (Get-Item -Path "./temp/cache" -ErrorAction Ignore) { # todo - rewrite with Test-Path
 		Write-Host "Old cache : found`n"
 	} else {
@@ -77,18 +78,22 @@ if (!$CleanUp -and $config.use_assets_cache) {
 	Write-Host "DONE`n"
 }
 
-# Creation of temp directory should be unconditional , in case user delete it manually
-New-Item -Path "temp" -ItemType Directory -ErrorAction Ignore > $null
-
 Write-Host "Cleaning up old builds ..."
 Remove-Item -Path "./temp/output" -Recurse -Force -ErrorAction Ignore
 Remove-Item -Path "./output_vm" -Recurse -Force -ErrorAction Ignore
 Remove-Item -Path "./output_yyc" -Recurse -Force -ErrorAction Ignore
 Write-Host "DONE`n"
 
+if ($CleanUp) {
+	exit
+}
+
+# Creation of temp directory should be unconditional , in case user delete it manually
+New-Item -Path "temp" -ItemType Directory -ErrorAction Ignore > $null
+
 $note_about_intentional_error = "`nPlease , don't mind the `"Empty file name is not legal`" error .`nAs far as i can tell , this is the only way to make gamemaker skip zipping of files :)"
 
-if (!$CleanUp -and $CompileYYC) {
+if ($CompileYYC) {
 	# Igor.exe is hardcoded to read location of VsDevCmd.bat from this file
 	Set-Content -NoNewline -Path "./temp/local_settings.json" -Value ("{ `"machine.Platform Settings.Windows.visual_studio_path`": `"" + $config.visual_studio_tools + "`" }")
 }
@@ -127,15 +132,13 @@ try {
 	Set-Location ".."
 }
 
-if (!$CleanUp) {
-	Write-Host "Adding additional content ..."
-	if ($CompileVM) {
-		addAdditionalContent -destination "./output_vm/" -content_paths $config.additional_directories_to_include
-	}
-	if ($CompileYYC) {
-		addAdditionalContent -destination "./output_yyc/" -content_paths $config.additional_directories_to_include
-	}
-	Write-Host "DONE`n"
+Write-Host "Adding additional content ..."
+if ($CompileVM) {
+	addAdditionalContent -destination "./output_vm/" -content_paths $config.additional_directories_to_include
 }
+if ($CompileYYC) {
+	addAdditionalContent -destination "./output_yyc/" -content_paths $config.additional_directories_to_include
+}
+Write-Host "DONE`n"
 
 #endregion Main script part
